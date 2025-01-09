f7799bded764873f1ef39b43d548fa931848a186
Abdullah Mamun <abdullah.mamun.0110@gmail.com>
2025-01-09 10:53:27 +0600

    revalidate next.js cache

--- src/services/feedback.service.js
+++ src/services/feedback.service.js
@@ -2,8 +2,8 @@
 import { GetStaticPropsContext } from 'next';
 
 const fetchFeedback = async () => {
- 
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/feedbacks`)
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/feedbacks`, { next: { revalidate: 10 } })
     const data = await response.json()
 
     return data.data || [];
--- src/services/work.service.js
+++ src/services/work.service.js
@@ -2,7 +2,7 @@
 
 const fetchWorks = async () => {
 
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/works`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/works`,{ next: { revalidate: 10 } });
     const data = await response.json();
     return data.data
 }
