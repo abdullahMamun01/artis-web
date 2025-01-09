9f460118d2aa7e1cd4bdc5333709cacaf0e2733d
Abdullah Mamun <abdullah.mamun.0110@gmail.com>
2025-01-09 10:56:10 +0600

    revalidate cache ever 1 sec added

--- src/services/feedback.service.js
+++ src/services/feedback.service.js
@@ -3,7 +3,7 @@ import { GetStaticPropsContext } from 'next';
 
 const fetchFeedback = async () => {
 
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/feedbacks`, { next: { revalidate: 10 } })
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/feedbacks`, { next: { revalidate: 1 } })
     const data = await response.json()
 
     return data.data || [];
